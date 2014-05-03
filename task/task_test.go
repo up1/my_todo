8b90a793cc409ac2c2710285e3c590eeb55f9e98
Somkiat <somkiat.p@gmail.com>
2014-05-04 01:09:03 +0700

    Add test case 2 :: create new task

--- task/task_test.go
+++ task/task_test.go
@@ -8,3 +8,14 @@ func TestNewTaskWithEmptyTitle(t *testing.T) {
 		t.Error("Expected 'Empty Title', error, got nil")
 	}
 }
+
+func TestNewTask(t *testing.T) {
+	title := "My Title"
+	task, _ := NewTask(title)
+	if task.Title != title {
+		t.Errorf("Expected title %q, got %q", title, task.Title)
+	}
+	if task.Done {
+		t.Errorf("New task is done")
+	}
+}
